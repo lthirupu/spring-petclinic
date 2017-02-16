7fce1f0740bc6e1060ff57f4dd9e734be3eb07b5
Lavanya Thirupur <lavanyalatha@Lavanyas-MacBook-Air.local>
2017-02-16 15:23:21 -0500

    Added test cases

--- src/test/groovy/org/springframework/samples/petclinic/model/ValidatorSpockTests.groovy
+++ src/test/groovy/org/springframework/samples/petclinic/model/ValidatorSpockTests.groovy
@@ -44,12 +44,98 @@ public class ValidatorSpockTests extends Specification {
 	}
 	
 	// 2. TODO: validate against null first name.
+	def "first name cannot be null"() {
+	  setup:
+	  def person = new Person();
+	  def validator = createValidator();
+	  
+	  when:
+	  person.setFirstName(null)
+	  person.setLastName("smith")
+      Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
+	  
+	  then:
+      constraintViolations.size() == 1;
+      ConstraintViolation<Person> violation = constraintViolations.iterator().next();
+	  violation.getPropertyPath().toString().equals("firstName");
+	  violation.getMessage().equals("may not be empty");
+	}
 	
 	// 3. TODO: validate a against a valid first name (non-empty, non-null value).
+	def "first name validation"() {
+	  setup:
+	  mockForConstraintsTests(Person);
+	  
+	  
+	  when:
+	  def person = new Person(lastname: "Smith");
+	  person.firstName = firstName; 
+	  person.validate();
+	  
+	  then:
+	  person.hasErrors() == !valid
 
+	  where:
+	  firstName     || valid
+	  " "			|| false
+	  null          || false
+	  "Will"        || true
+	}
 	// 4. TODO: validate against empty last name
+	def "last name cannot be empty"() {
+	  setup:
+	  def person = new Person();
+	  def validator = createValidator();
+	  
+	  when:
+	  person.setFirstName("will")
+	  person.setLastName("")
+      Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
+	  
+	  then:
+      constraintViolations.size() == 1;
+      ConstraintViolation<Person> violation = constraintViolations.iterator().next();
+	  violation.getPropertyPath().toString().equals("lastName");
+	  violation.getMessage().equals("may not be empty");
+	}
 
 	// 5. TODO: validate against null last name
+	def "last name cannot be null"() {
+	  setup:
+	  def person = new Person();
+	  def validator = createValidator();
+	  
+	  when:
+	  person.setFirstName("will")
+	  person.setLastName(null)
+      Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
+	  
+	  then:
+      constraintViolations.size() == 1;
+      ConstraintViolation<Person> violation = constraintViolations.iterator().next();
+	  violation.getPropertyPath().toString().equals("lastName");
+	  violation.getMessage().equals("may not be empty");
+	}
 	
 	// 6. TODO: validate a against a valid last name (non-empty, non-null value).
+	def "last name validation"() {
+	  setup:
+	  mockForConstraintsTests(Person);
+	  def person = new Person();
+	  def validator = createValidator();
+	  
+	  when:
+	  person.lastName = lastName;
+	  person.firstName("Will");	 
+	  validator.validate(person);
+	  
+	  then:
+	  person.hasErrors() == !valid
+
+	  where:
+	  lastName      || valid
+	  " "			|| false
+	  null          || false
+	  "Smith"       || true
+	}
 }
